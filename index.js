0c4a2f87aa30ae49daffa93b7ef5a3eb920d8b3c
Quinn6182 <95182982+Quinn6182@users.noreply.github.com>
2023-02-19 05:59:42 -0700

    Added the actual javascript files

---
+++ index.js
@@ -0,0 +1,30 @@
+const socket = require('ws')
+const say = require('say')
+const Logger = require('./logger')
+var clients = {"connections": {}}
+var done = false
+const webSocket = new socket.Server({port: 8001})
+webSocket.on("connection", wsClient => {
+    wsClient.on('message', messageData => {
+        if (messageData.toString() == "__CONNECTIONS_DONE__") {
+            done = true
+            say.speak("Connections Finished")
+        } else {
+            if (clients.connections.hasOwnProperty(messageData.toString()) == false && done == false) {
+                Logger.logInfo("Computer "+messageData.toString()+" connected")
+                clients.connections[messageData.toString()] = wsClient
+                say.speak("Computer : "+messageData.toString()+" Connected")
+            } else { 
+                var messageInfo = messageData.toString().split("|")
+                Logger.logInfo("Routing message "+messageInfo[1]+" to "+messageInfo[0]+".")
+                setTimeout(() => {clients.connections[messageInfo[0]].send(messageInfo[1])}, 1500)
+                
+            }
+        }
+    })
+
+    wsClient.on('close', () => {
+        say.speak("Client Disconnected!")
+        Logger.logInfo("Client Disconnected!")
+    })
+})
---
+++ logger.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+exports.logInfo = function(to_log) {
+    console.log("[INFO] "+to_log)
+    fs.appendFileSync("log.txt", "[INFO] "+to_log+"\n")
+}
